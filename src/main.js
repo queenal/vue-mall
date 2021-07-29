48c86acf0df052d501ad3f471728e849fd7e7c69
rjman <rjman.ljm@gmail.com>
2021-07-29 22:13:49 +0800

    Import amap

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/base.scss'
 import router from "./router";
+import VueAMap from 'vue-amap';
+
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+    key: 'your amap key',
+    plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+    // 默认高德 sdk 版本为 1.4.4
+    v: '1.4.4'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  router
+    render: h => h(App),
+    router
 }).$mount('#app')
